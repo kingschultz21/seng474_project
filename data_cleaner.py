2fce68dc76ddd849c8250fab2976d094a402c5dd
Connor Schultz <connor.schultz@gmail.com>
2019-11-18 16:32:27 -0800

    added new stuff from monday meeting

--- data_cleaner.py
+++ data_cleaner.py
@@ -9,11 +9,18 @@ if(__name__ == '__main__'):
 	fname = cwd + "/car_data/fullspecs.csv"		#create car dataset filename
 	attrname = cwd + "/car_data/attrs.txt"		#create car attributes filename
 	outname = cwd + "/car_data/proc_cars.csv"
-
+	print("Loading :"+fname)
 	cars = pd.read_csv(fname, index_col=0, header=None, dtype = object).T		#transpose of csv for simpler usage
+	cars = cars.apply(lambda x: x.astype(object).str.lower())
+	print("Done Loading :"+fname)
+
+	print("Processing and Creating: "+outname)
 	attrs = [line.rstrip('\n') for line in open(attrname)]						#extract relevant attributes
 	cars = cars[attrs]															#remove non-relevant attributes from df
-	cars = cars.dropna(subset = ['MSRP'])										#remove any in specific columns
+	cars = cars.dropna()														#remove na values
+
 	cars['MSRP'] = cars.MSRP.replace('\D', '', regex=True).astype(int)
+	cars['Drivetrain'] = cars['Drivetrain'].replace('[^A-Za-z0-9]+','',regex=True)
 
 	cars.to_csv(outname)														#export processed dataset
+	print("Done Processing and Creating: "+outname)
