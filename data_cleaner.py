2014e8ccf1b3a033fedba1c0fd5685f1fc95884d
Connor Schultz <connor.schultz@gmail.com>
2019-11-18 21:19:31 -0800

    updated data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -4,23 +4,34 @@
 import pandas as pd
 import os
 
-if(__name__ == '__main__'):
-	cwd = os.getcwd()							#get current working directory
-	fname = cwd + "/car_data/fullspecs.csv"		#create car dataset filename
-	attrname = cwd + "/car_data/attrs.txt"		#create car attributes filename
-	outname = cwd + "/car_data/proc_cars.csv"
-	print("Loading :"+fname)
-	cars = pd.read_csv(fname, index_col=0, header=None, dtype = object).T		#transpose of csv for simpler usage
-	cars = cars.apply(lambda x: x.astype(object).str.lower())
-	print("Done Loading :"+fname)
+def info_print(ftype, df):
+	print("number of rows in "+ftype+" file: "+ str(len(df)))
+	print("number of columns in "+ftype+" file: "+str(len(df.columns)))
+	print("number of na values in "+ftype+" file: "+str(df.isna().sum().sum()))
 
-	print("Processing and Creating: "+outname)
-	attrs = [line.rstrip('\n') for line in open(attrname)]						#extract relevant attributes
-	cars = cars[attrs]															#remove non-relevant attributes from df
-	cars = cars.dropna()														#remove na values
+def main():
+	cwd = os.getcwd()						#get current working directory
+	fname = "/car_data/fullspecs.csv"		#create car dataset filename
+	attrname = "/car_data/attrs.txt"		#create car attributes filename
+	outname = "/car_data/proc_cars.csv"
+
+	print("loading :"+fname)
+	cars = pd.read_csv((cwd+fname), index_col=0, header=None, dtype = object).T		#transpose of csv for simpler usage
+	cars = cars.apply(lambda x: x.astype(object).str.lower())
+	print("done loading :"+fname)
+	info_print("input",cars)
+
+	print("processing "+fname+" and creating "+outname)
+	attrs = [line.rstrip('\n') for line in open(cwd+attrname)]						#extract relevant attributes
+	cars = cars[attrs]																#remove non-relevant attributes from df
+	cars = cars.dropna()															#remove na values
 
 	cars['MSRP'] = cars.MSRP.replace('\D', '', regex=True).astype(int)
-	cars['Drivetrain'] = cars['Drivetrain'].replace('[^A-Za-z0-9]+','',regex=True)
+	cars = cars.replace('[^A-Za-z0-9]+','',regex=True)
 
-	cars.to_csv(outname)														#export processed dataset
-	print("Done Processing and Creating: "+outname)
+	cars.to_csv(cwd+outname)														#export processed dataset
+	print("done processing "+fname+" and done creating "+outname)
+	info_print("output",cars)
+
+if(__name__ == '__main__'):
+	main()
