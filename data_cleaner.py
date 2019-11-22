573976ce7cbb9e8ba6aa0ad4e08799102d1dab00
Connor Schultz <connor.schultz@gmail.com>
2019-11-22 14:04:05 -0800

    cleaned up a bit

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,11 +1,11 @@
 #
 # Exports a processed version of the 'fullspecs.csv' dataset
 #
+
 import pandas as pd
 import os
 import numpy as np
 pd.options.mode.chained_assignment = None #supress SettingWithCopyWarning (problematic?)
-
 #
 # process 'Drivetrain' attrs and rename column
 #
@@ -157,21 +157,27 @@ def threshold(df, t):
 		df[col].loc[df[col].isin(to_remove)] = np.nan
 		df = df.dropna()
 	return df
-
+#
+# print basic info about dateframes (csv's)
+#
 def info_print(ftype, df):
 	print("number of rows in "+ftype+" file: "+ str(len(df)))
 	print("number of columns in "+ftype+" file: "+str(len(df.columns)))
 	print("number of na values in "+ftype+" file: "+str(df.isna().sum().sum()))
-
+#
+# 
+# data_cleaner.py: converts the original 'fullspecs.csv' dataset into a usable format for regression
+#
+#
 def main():
-	cwd = os.getcwd()						#get current working directory
-	fname = "/car_data/fullspecs.csv"		#create car dataset filename
-	attrname = "/car_data/attrs.txt"		#create car attributes filename
-	outname = "/car_data/proc_cars.csv"
+	cwd = os.getcwd()																#get current working directory
+	fname = "/car_data/fullspecs.csv"												#create car dataset filename
+	attrname = "/car_data/attrs.txt"												#create car attributes filename
+	outname = "/car_data/proc_cars.csv"												#create output dataset filename
 
 	print("LOADING :"+fname)
 	cars = pd.read_csv((cwd+fname), index_col=0, header=None, dtype = object).T		#transpose of csv for simpler usage
-	cars = cars.apply(lambda x: x.astype(object).str.lower())
+	cars = cars.apply(lambda x: x.astype(object).str.lower())						#convert everything to lower case
 	print("DONE LOADING :"+fname)
 
 	info_print("input",cars)
@@ -179,9 +185,9 @@ def main():
 	attrs = [line.rstrip('\n') for line in open(cwd+attrname)]						#extract relevant attributes
 	cars = cars[attrs]																#remove non-relevant attributes from df
 	cars = cars.dropna()															#remove na values
-	cars = cars.replace('[^A-Za-z0-9.]+','',regex=True)
+	cars = cars.replace('[^A-Za-z0-9.]+','',regex=True)								#remove special characters
 
-	cars = process_cars(cars)
+	cars = process_cars(cars)														#process cars!
 	print("DONE PROCESSING: "+fname)
 
 	print("ENCODING: "+outname)
@@ -189,7 +195,7 @@ def main():
 	cars = label_encode(cars)														#label endcoding scheme
 	print("DONE ENCODING: "+outname)
 
-	cars = threshold(cars, 2)
+	cars = threshold(cars, 2)														#threshold based on value
 	cars.to_csv(cwd+outname)														#export processed dataset
 	print("DONE CREATING: "+outname)
 	info_print("output",cars)
